0d5a26a962515e65845b1aa6455e7b9a9c62a38a
karlarobinson <karlarobinson5@gmail.com>
2016-11-14 18:28:07 -0600

    add photo mounter to photo model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,5 +3,12 @@ class Photo < ApplicationRecord
 	validates :url, uniqueness: true
 	has_many :locations
 	has_many :pages, through: :locations
+	# validate :image_size
 
+	mount_uploader :image, ImageUploader
+	 
+	# private
+	#   def image_size
+	#     errors[:image] << "should be less than 500KB" if image.size > 0.5.megabytes
+	#   end
 end
