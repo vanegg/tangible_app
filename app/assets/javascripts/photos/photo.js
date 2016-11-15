8c3d287d8a7b9b8ada1924f094fac8a75aea5221
karlarobinson <karlarobinson5@gmail.com>
2016-11-15 17:37:05 -0600

    generalize shared/_errors template

---
+++ app/assets/javascripts/photos/photo.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+  $(".container").on("click", "#upload", function(e){
+    e.preventDefault();
+    // console.log($("#photo_photo"));
+    // if($("#photo_photo").val() == nil){
+      alert("empty")
+    // }else{
+    //   alert("not empty")
+    // };
+  });
+});
