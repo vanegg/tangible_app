9af266eb8825636cb7d2f197fceaf7dfa47f27be
karlarobinson <karlarobinson5@gmail.com>
2016-11-17 11:07:35 -0600

    change fog version

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -11,8 +11,8 @@ CarrierWave.configure do |config|
       :provider              => 'AWS',
       :aws_access_key_id     => ENV['S3_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET'],
-      :region                => ENV['S3_REGION'],
-      :aws_signature_version => 2
+      :region                => ENV['S3_REGION']
+      # :aws_signature_version => 2
     }
   else
     config.storage = :file
@@ -28,7 +28,7 @@ CarrierWave.configure do |config|
   end
 
   config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
-  config.fog_directory    = ENV['S3_BUCKET_NAME']
+  config.fog_directory = ENV['S3_BUCKET_NAME']
   
   # config.s3_access_policy = :public_read                          # Generate http:// urls. Defaults to :authenticated_read (https://)
   # config.fog_host         = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET_NAME']}"
