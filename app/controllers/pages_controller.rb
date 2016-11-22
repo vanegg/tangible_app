80a25f7f3da9a2a67d6ab81815325bb3d8a3ac86
Vanessa <van.gg@live.com.mx>
2016-11-22 10:21:12 -0600

    Fix css

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -21,11 +21,10 @@ class PagesController < ApplicationController
   			return
   		end
   		current_page = Page.where(page_num: current_page_id, album_id: album.id)
-  		@page_left = Page.where(page_num: current_page_id + 1, album_id: album.id)
-  		@page_right = Page.where(page_num: current_page_id + 2, album_id: album.id)
+  		p @page_left = Page.where(page_num: current_page_id + 1, album_id: album.id)
+  		p @page_right = Page.where(page_num: current_page_id + 2, album_id: album.id)
   	end
-
+    p '*' * 50
+    p @page_left.layout
   end
-
-
 end
