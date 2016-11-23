29163563a380d75aed1e99a290cd74a57ffe574f
Vanessa <van.gg@live.com.mx>
2016-11-23 17:23:51 -0600

    Save dropped photos in DB

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -23,12 +23,26 @@ class PhotosController < ApplicationController
 	end
 
   def addpage
-    p '*'*30
-    p params
+
     album = Album.find(params[:album])
     page = album.pages.find_by(page_num: params[:num_page])
     page_place = params[:page_place]
     url = params[:url]
+
+    unless url.match('\/\d+\/').nil?
+      photo_id = url.match('\/\d+\/')[0].gsub('/','').to_i
+      photo = Photo.find(photo_id)
+    end
+
+    if photo 
+      location = Location.create(photo_id: photo_id, page_id: page.id, page_place: page_place)
+      photo.locations << location
+      photo.save
+    end
+    p '*' * 30
+    p photo.pages
+    p page.photos
+    p location
   end
 
 	private
