3f1db728f3e5ed9f7168c8bd5d1efffd10c2cae8
karlarobinson <karlarobinson5@gmail.com>
2016-11-22 19:15:25 -0600

    page layouts are saved

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -7,12 +7,13 @@ $(document).ready(function(ev){
   $('.select-layout li').on('click', function(){
     album = $('#num_album').text();
     layout = $(this).index() + 1;
-    page_num = $(this).parent().parent().attr('id');
+    page_num = $(this).children().attr('id');
+    console.log(page_num);
     lay1 = '<div class="container-fluid"><div class="row board-container layout_1"><div class="photoboard"></div></div></div>'
     lay2 = '<div class="container-fluid"><div class="row board-container layout_2"><div class="photoboard"></div><p></p><div class="photoboard"></div></div></div>'
     lay3 = '<div class="container-fluid"><div class="row board-container layout_3">'
     + '<div class="photoboard"></div><div class="photoboard"></div><br><div class="photoboard"></div><div class="photoboard"></div></div></div>'
     $(this).children().attr('href','/updatelayout?album=' + album + '&page_num=' + page_num + "&layout=" + layout);
-    $(this).parent().replaceWith(lay3);
+    $(this).parent().parent().replaceWith(eval("lay" + layout));
   });
 });
