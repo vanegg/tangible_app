8b8fa925c9e6605a9472f8ff7a82417dea10be76
karlarobinson <karlarobinson5@gmail.com>
2016-11-23 09:43:32 -0600

    adjust js for carrousel

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -4,16 +4,32 @@ $(document).ready(function(ev){
   	console.log("rigth button pushed");
   });
 
+  // listening to a lyout selection
+  // replaces layout option with layout selected
+  // stores page layout selection in database
   $('.select-layout li').on('click', function(){
     album = $('#num_album').text();
     layout = $(this).index() + 1;
     page_num = $(this).children().attr('id');
-    console.log(page_num);
     lay1 = '<div class="container-fluid"><div class="row board-container layout_1"><div class="photoboard"></div></div></div>'
     lay2 = '<div class="container-fluid"><div class="row board-container layout_2"><div class="photoboard"></div><p></p><div class="photoboard"></div></div></div>'
     lay3 = '<div class="container-fluid"><div class="row board-container layout_3">'
     + '<div class="photoboard"></div><div class="photoboard"></div><br><div class="photoboard"></div><div class="photoboard"></div></div></div>'
     $(this).children().attr('href','/updatelayout?album=' + album + '&page_num=' + page_num + "&layout=" + layout);
     $(this).parent().parent().replaceWith(eval("lay" + layout));
+    // loadPhotos(page_num);
   });
+
+
+  $( "body" ).on( "click", ".carousel-control", function() {
+    console.log($( this ).attr('id'));
+    page = $('.item.active').children().children().children().children().first().attr('id');
+    album = $('#num_album').text();
+    $('#' + $( this ).attr('id')).attr('href','/newpage?album=' + album + '&page=' + page + "&type=" + $( this ).attr('id'));
+  });
+
+  // // load photos
+  // function loadPhotos(page_num){
+    
+  // };
 });
