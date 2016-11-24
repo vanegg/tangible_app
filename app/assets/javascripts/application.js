9454a4bf90dbc244fb3ccb63c73994330fd5d799
karlarobinson <karlarobinson5@gmail.com>
2016-11-23 19:46:47 -0600

    delete js after next page is clicked

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,14 +17,12 @@
 //= require_tree .
 
 $(document).ready(function(ev){
-	var items = $(".nav li").length;
-    var leftRight=0;
-    if(items>5){
-        leftRight=(items-5)*50*-1;
-    }
+	// var items = $(".nav li").length;
+ //    var leftRight=0;
+ //    if(items>5){
+ //        leftRight=(items-5)*50*-1;
+ //    }
     $('#custom_carousel').on('slide.bs.carousel', function (evt) {
-        
- 
       $('#custom_carousel .controls li.active').removeClass('active');
       $('#custom_carousel .controls li:eq('+$(evt.relatedTarget).index()+')').addClass('active');
     })
