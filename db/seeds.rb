f2955c2edaf7157e3dafe6a337d7656ff7d4cd19
Vanessa <van.gg@live.com.mx>
2016-11-23 19:48:26 -0600

    Modified seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,16 +26,16 @@ album.save
   page = Page.new(layout: 0, page_num: n + 1)
   album.pages << page
 
-  5.times do |n|
-    url = 'http://url.com/'
-    url = url + ('a'..'z').to_a.shuffle[0,8].join
-    photo = Photo.new(photo: url)
-    location = Location.create(photo_id: photo.id, page_id: page.id, page_place: 1)
+  # 5.times do |n|
+  #   url = 'http://url.com/'
+  #   url = url + ('a'..'z').to_a.shuffle[0,8].join
+  #   photo = Photo.new(photo: url)
+  #   location = Location.create(photo_id: photo.id, page_id: page.id, page_place: 1)
 
-    User.find(1).photos << photo
-    photo.locations << location
-    photo.save
-  end
+  #   User.find(1).photos << photo
+  #   photo.locations << location
+  #   photo.save
+  # end
 
   page.save
 end
