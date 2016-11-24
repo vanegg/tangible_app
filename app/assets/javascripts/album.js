5ba267efd30273ccff5d49191d2c93af82156545
Vanessa <van.gg@live.com.mx>
2016-11-24 16:47:43 -0600

    Saving title

--- app/assets/javascripts/album.js
+++ app/assets/javascripts/album.js
@@ -1,6 +1,25 @@
 $(document).ready(function(ev){
  
-    doDraggable('.draggable');
-    doDroppable('.photoboard');
+  doDraggable('.draggable');
+  doDroppable('.photoboard');
+
+  $('#album_title').on('change', function(event){
+    console.log($('#album_title').val());
+
+  });
+
+  num_album = $('#num_album').text();
+  $('#edit_album_' + num_album).on('submit', function(event){
+  	event.preventDefault();
+    console.log($('#album_title').val());
+  	
+  });
+
 });
 
+function saveTitle(){
+	title = $('#album_title').val();
+	$.post('/save_title',title, function(resp) {
+    console.log('callback:' + resp);
+  });
+};
