df9286817312f13b5ca605ec8eb150e881b39be4
karlarobinson <karlarobinson5@gmail.com>
2016-11-28 08:44:44 -0600

    default title for albums is album sin titulo

--- db/migrate/20161114195120_create_albums.rb
+++ db/migrate/20161114195120_create_albums.rb
@@ -1,7 +1,7 @@
 class CreateAlbums < ActiveRecord::Migration[5.0]
   def change
     create_table :albums do |t|
-    	t.string :title
+    	t.string :title, default: "Album Sin Titulo"
     	t.belongs_to :user
     	t.timestamps
     end
