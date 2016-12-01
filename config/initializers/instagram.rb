287804522d22bd7063fba975c8ac79d181f3210c
karlarobinson <karlarobinson5@gmail.com>
2016-12-01 09:35:23 -0600

    update website name

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -2,6 +2,7 @@ require "instagram"
 
 CALLBACK_URL = "https://realidadtangible.herokuapp.com/instagram/callback"
 
+
 Instagram.configure do |config|
   config.client_id     = ENV["INSTA_CLIENT_ID"]
   config.client_secret = ENV["INSTA_SECRET"]
