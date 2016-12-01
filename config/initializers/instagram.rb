d8a395270f8f40addc53388435ec2be2c374fa6b
karlarobinson <karlarobinson5@gmail.com>
2016-12-01 10:16:22 -0600

    correct grammar

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -2,7 +2,6 @@ require "instagram"
 
 CALLBACK_URL = "https://realidadtangible.herokuapp.com/instagram/callback"
 
-
 Instagram.configure do |config|
   config.client_id     = ENV["INSTA_CLIENT_ID"]
   config.client_secret = ENV["INSTA_SECRET"]
